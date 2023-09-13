001df7a9a86ac38e27bc714ae978144dde497d0a
MOUSSA THIOR <netrathior@gmail.com>
2023-09-13 14:56:19 +0000

    Add Controller Compte class test

--- src/test/java/com/wallet/wallet_msir_jour_groupe2/controller/CompteControllerTest.java
+++ src/test/java/com/wallet/wallet_msir_jour_groupe2/controller/CompteControllerTest.java
@@ -3,7 +3,6 @@ package com.wallet.wallet_msir_jour_groupe2.controller;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
-
 class CompteControllerTest {
 
     @Test
