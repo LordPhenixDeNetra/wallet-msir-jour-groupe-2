c28bf7a49688951b93f6bbd85deda0143c3d9c01
MOUSSA THIOR <netrathior@gmail.com>
2023-09-13 15:50:50 +0000

    Add CompteResourceTest

---
+++ src/test/java/com/wallet/wallet_msir_jour_groupe2/rest/CompteResourceTest.java
@@ -0,0 +1,28 @@
+package com.wallet.wallet_msir_jour_groupe2.rest;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CompteResourceTest {
+
+    @Test
+    void getAllComptes() {
+    }
+
+    @Test
+    void getCompte() {
+    }
+
+    @Test
+    void createCompte() {
+    }
+
+    @Test
+    void updateCompte() {
+    }
+
+    @Test
+    void deleteCompte() {
+    }
+}
