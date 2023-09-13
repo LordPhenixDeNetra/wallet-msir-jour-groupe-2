1440089fa1caa93475c8a0e33758aaeec7f982e2
MOUSSA THIOR <netrathior@gmail.com>
2023-09-13 15:45:55 +0000

    Add Controller Test Files

---
+++ src/test/java/com/wallet/wallet_msir_jour_groupe2/controller/HomeControllerTest.java
@@ -0,0 +1,12 @@
+package com.wallet.wallet_msir_jour_groupe2.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HomeControllerTest {
+
+    @Test
+    void index() {
+    }
+}
