a3e36f25a1b4dc75d16568b73a01cb01cdee5c5a
Mamadou <senemamadou1999@gmail.com>
2023-09-17 00:11:27 +0000

    ajout de la methode findAllTransaction pour obtenir la liste des transactions

--- src/main/java/com/wallet/wallet_msir_jour_groupe2/repos/TransactionRepository.java
+++ src/main/java/com/wallet/wallet_msir_jour_groupe2/repos/TransactionRepository.java
@@ -5,11 +5,14 @@ import com.wallet.wallet_msir_jour_groupe2.domain.Transaction;
 import com.wallet.wallet_msir_jour_groupe2.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
 
     Transaction findFirstByUser(User user);
 
     Transaction findFirstByCompte(Compte compte);
+    //List<Transaction> findAllTransaction();
 
 }
