9ec8127bf25519ef6302beca460a1074f2e18eba
Mamadou <senemamadou1999@gmail.com>
2023-09-17 00:18:31 +0000

    ajout des test unitaires pour la classe TransactionService

--- src/test/java/com/wallet/wallet_msir_jour_groupe2/service/TransactionServiceTest.java
+++ src/test/java/com/wallet/wallet_msir_jour_groupe2/service/TransactionServiceTest.java
@@ -1,4 +1,83 @@
+package com.wallet.wallet_msir_jour_groupe2.service;
+
+import com.wallet.wallet_msir_jour_groupe2.domain.Compte;
+import com.wallet.wallet_msir_jour_groupe2.domain.Transaction;
+import com.wallet.wallet_msir_jour_groupe2.domain.User;
+import com.wallet.wallet_msir_jour_groupe2.model.*;
+import com.wallet.wallet_msir_jour_groupe2.repos.CompteRepository;
+import com.wallet.wallet_msir_jour_groupe2.repos.TransactionRepository;
+import com.wallet.wallet_msir_jour_groupe2.repos.UserRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
 class TransactionServiceTest {
-  
+    @Mock
+    TransactionRepository transactionRepository;
+    @InjectMocks
+    TransactionService transactionService;
+    @Autowired
+    UserRepository userRepository;
+    @Autowired
+    CompteRepository compteRepository;
+    Compte compte;
+    User user;
+    @BeforeEach
+    void setUp()
+    {
+        compte=new Compte(20000.0, TypeStatutCompte.ACTIF);
+        user=new User("SENE","Mamadou", TypeUser.CUSTOMER,"778340335",compte);
+        compteRepository.save(compte);
+        userRepository.save(user);
+    }
+
+    @Test
+    void findAll() {
+        List<Transaction> initlistTransaction= List.of(
+                new Transaction(1000.0, TypeTransaction.DEPOT, TypeStatutTransaction.REUSSITE,user.getId(),user,compte),
+                new Transaction(2000.0, TypeTransaction.RETRAIT, TypeStatutTransaction.ENCOUR,user.getId(),user,compte)
+        );
+        when(transactionRepository.findAll()).thenReturn(initlistTransaction);
+        List<TransactionDTO> listtdo= transactionService.findAll();
+        assertEquals(initlistTransaction.size(),listtdo.size());
+    }
+
+    @Test
+    void get() {
+        /*Transaction t=new Transaction(1000.0, TypeTransaction.DEPOT, TypeStatutTransaction.REUSSITE,user.getId(),user,compte);
+        transactionRepository.save(t);
+        when(transactionRepository.findById(t.getId())).thenReturn(null);
+        TransactionDTO transactionDTO=transactionService.get(t.getId());
+        assertEquals(t.getId(),transactionDTO.getId());*/
+    }
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void delete() {
+    }
+    @AfterEach
+    void tearDown()
+    {
+        userRepository.deleteAll();
+        compteRepository.deleteAll();
+
+    }
 }
