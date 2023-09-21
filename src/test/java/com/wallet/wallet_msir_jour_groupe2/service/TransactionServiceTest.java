b98ec98174c9029c589a90d5645038e11b503b33
Mamadou <senemamadou1999@gmail.com>
2023-09-21 23:38:49 +0000

    ajout des methodes de simulation pour tester les methodes du service transaction

--- src/test/java/com/wallet/wallet_msir_jour_groupe2/service/TransactionServiceTest.java
+++ src/test/java/com/wallet/wallet_msir_jour_groupe2/service/TransactionServiceTest.java
@@ -13,14 +13,15 @@ import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
-
 import java.util.List;
 import java.util.Optional;
-
-import static org.junit.jupiter.api.Assertions.*;
+import static java.util.Optional.*;
 import static org.mockito.Mockito.when;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+import com.wallet.wallet_msir_jour_groupe2.model.TransactionDTO;
 
 @SpringBootTest
 class TransactionServiceTest {
@@ -58,8 +59,8 @@ class TransactionServiceTest {
     @Test
     void get() {
         Transaction t=new Transaction(1000.0, TypeTransaction.DEPOT, TypeStatutTransaction.REUSSITE,user.getId(),user,compte);
-        transactionRepository.save(t);
-        Optional<Transaction> t2= Optional.of(new Transaction(1000.0, TypeTransaction.RETRAIT, TypeStatutTransaction.ANNULER,user.getId(),user,compte));
+        Optional<Transaction> t2= of(new Transaction(1000.0, TypeTransaction.RETRAIT, TypeStatutTransaction.ANNULER,user.getId(),user,compte));
+        t2.get().setId(1L);
         when(transactionRepository.findById(t.getId())).thenReturn(t2);
         TransactionDTO transactionDTO=transactionService.get(t.getId());
         assertEquals(t.getMontantTransaction(),transactionDTO.getMontantTransaction());
@@ -67,36 +68,40 @@ class TransactionServiceTest {
 
     @Test
     void create() {
-        Transaction t=new Transaction(1000.0, TypeTransaction.DEPOT, TypeStatutTransaction.REUSSITE,user.getId(),user,compte);
-        transactionRepository.save(t);
-        when(transactionRepository.findById(t.getId())).thenReturn(Optional.of(t));
-        final TransactionDTO transactionDTO=transactionService.get(t.getId());
-        //ici cette ligne en dessous qui aurrait retourner l'id de la transaction, retourne null
-        //alors la methode create renvoi null
-        //Long idtransaction=transactionService.create(transactionDTO);
-        //Cependant TransactionDTO est non null dans le context actuel
-        assertNotNull(transactionDTO);
+        TransactionDTO transactionDTO = new TransactionDTO();
+        transactionDTO.setMontantTransaction(1000.0);
+        // Créer un objet Transaction simulé qui sera retourné par le repository
+        Transaction simulatedTransaction = new Transaction(1000.0, TypeTransaction.DEPOT, TypeStatutTransaction.REUSSITE,user.getId(),user,compte);
+        simulatedTransaction.setId(1L);
+
+        // Définir le comportement simulé du repository
+        when(transactionRepository.save(any(Transaction.class))).thenReturn(simulatedTransaction);
+        Long result = transactionService.create(transactionDTO);
+
+        // Vérifier que la méthode save du repository a été appelée avec le bon argument
+        verify(transactionRepository).save(any(Transaction.class));
+        assertEquals(1L,result);
     }
 
     @Test
     void update() {
         Transaction t=new Transaction(1000.0, TypeTransaction.DEPOT, TypeStatutTransaction.REUSSITE,user.getId(),user,compte);
-        transactionRepository.save(t);
-        //Transaction transaction=new Transaction(7000.0, TypeTransaction.TRANSFERT, TypeStatutTransaction.ANNULER,user.getId(),user,compte);
-        when(transactionRepository.findById(t.getId())).thenReturn(Optional.of(t));
+        when(transactionRepository.findById(t.getId())).thenReturn(of(t));
         final TransactionDTO transactionDTO=transactionService.get(t.getId());
         transactionService.update(t.getId(),transactionDTO);
+        verify(transactionRepository).save(any(Transaction.class));
 
     }
 
-  /*  @Test
+    @Test
     void delete() {
         //here we want to delete transaction via service
         Transaction transaction_deleted=new Transaction(1000.0, TypeTransaction.DEPOT, TypeStatutTransaction.REUSSITE,user.getId(),user,compte);
-        transactionRepository.save(transaction_deleted);
+        //transactionRepository.save(transaction_deleted);
+        when(transactionRepository.findById(transaction_deleted.getId())).thenReturn(null);
         transactionService.delete(transaction_deleted.getId());
-        assertNull(transaction_deleted);
-    }*/
+
+    }
     @AfterEach
     void tearDown()
     {
